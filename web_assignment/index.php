91162236ff5c64f07e519137e770b7c58a16c06e
Cuong111111 <luuchicuong18092004@gmail.com>
2025-04-13 17:10:19 +0700

    update permission

--- web_assignment/index.php
+++ web_assignment/index.php
@@ -21,7 +21,7 @@ if (!isset($_GET['page']) || empty($_GET['page'])) {
 }
 
 // Kiểm tra nếu người dùng chưa đăng nhập và trang yêu cầu đăng nhập
-$protectedPages = ['admin_dashboard','user_dashboard', 'profile', 'settings']; // Các trang yêu cầu đăng nhập
+$protectedPages = ['resetpssw']; // Các trang yêu cầu đăng nhập
 
 $page = isset($_GET['page']) ? $_GET['page'] : 'home';  
 $controllerFile = 'controllers/' . $page . '.php';
@@ -33,12 +33,12 @@ if (in_array($page, $protectedPages) && !isset($_SESSION['user_id'])) {
 }
 
 // Kiểm tra quyền truy cập (Ví dụ: chỉ admin mới có thể vào trang quản trị)
-$adminPages = ['admin', 'manage_users']; // Các trang chỉ dành cho admin
+$adminPages = ['admin', 'admin_create_test','admin_history','admin_manage_questions','admin_statistic','admin_view_question','adminProfile']; // Các trang chỉ dành cho admin
 if (in_array($page, $adminPages) && (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin')) {
     die("Access Denied. You do not have permission to access this page.");
 }
 
-$userPages = ['user_dashboard', 'manage_users']; // Các trang chỉ dành cho user
+$userPages = ['user', 'result','profile','doTest','user_history']; // Các trang chỉ dành cho user
 if (in_array($page, $userPages) && (!isset($_SESSION['role']) || $_SESSION['role'] !== 'user')) {
     die("Access Denied. You do not have permission to access this page.");
 }
